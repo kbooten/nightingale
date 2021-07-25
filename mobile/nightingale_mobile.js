71c2d9f3d2c6aa1553be5e1567358e60eb3380d2
kbooten <7179456+kbooten@users.noreply.github.com>
2021-07-25 10:27:03 -0400

    debugging

--- mobile/nightingale_mobile.js
+++ mobile/nightingale_mobile.js
@@ -596,8 +596,8 @@ function injectPopup(textNode){
 		var nRecentQuotes_ = JSON.parse(localStorage.getItem("nRecents"));
 		alert(nRecentQuotes_);
 		nRecentQuotes_.unshift(quote); // prepend
-		nRecentQuotes_ = nRecentQuotes_.slice(0,5); // limit size
-		localStorage(setItem("nRecents"),JSON.stringify(nRecentQuotes_));
+		// nRecentQuotes_ = nRecentQuotes_.slice(0,5); // limit size
+		// localStorage(setItem("nRecents"),JSON.stringify(nRecentQuotes_));
 		alert(nRecentQuotes_);
 		////
 		targetTextId+=1; //increment in case there is more than one on page (in case of Twitter)
